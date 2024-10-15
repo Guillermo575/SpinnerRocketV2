6d87931731ce36326ce4992f6a3d35729ca43e19
guillermo <guille_medina575@hotmail.com>
2024-10-15 01:05:24 -0600

    bezier path library

---
+++ SpinnerRocket/Assets/PathCreator/Scripts/GeneratePathExample.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+namespace PathCreation.Examples 
+{
+    [RequireComponent(typeof(PathCreator))]
+    public class GeneratePathExample : MonoBehaviour {
+        public Transform[] waypoints;
+        void Start () {
+            if (waypoints.Length > 0) {
+                var objPath = GetComponent<PathCreator>();
+                BezierPath bezierPath = new BezierPath (waypoints);
+                GetComponent<PathCreator> ().bezierPath = bezierPath;
+            }
+        }
+    }
+}
---
+++ SpinnerRocket/Assets/PathCreator/Scripts/PathFollower.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+namespace PathCreation
+{
+    public class PathFollower : MonoBehaviour
+    {
+        #region Variables
+        public PathCreator pathCreator;
+        public EndOfPathInstruction endOfPathInstruction;
+        public bool StartMove = true;
+        public float speed = 5;
+        public bool RotateObject = false;
+        float distanceTravelled;
+        #endregion
+
+        #region Start & Update
+        void Start()
+        {
+            UpdatePathFollow();
+        }
+        void Update()
+        {
+            if (pathCreator != null && StartMove)
+            {
+                distanceTravelled += speed * Time.deltaTime;
+                transform.position = pathCreator.path.GetPointAtDistance(distanceTravelled, endOfPathInstruction);
+                if(RotateObject)
+                {
+                    transform.rotation = pathCreator.path.GetRotationAtDistance(distanceTravelled, endOfPathInstruction);
+                }
+            }
+        }
+        #endregion
+
+        #region General
+        void OnPathChanged()
+        {
+            distanceTravelled = pathCreator.path.GetClosestDistanceAlongPath(transform.position);
+        }
+        public void UpdatePathFollow()
+        {
+            if (pathCreator != null)
+            {
+                pathCreator.pathUpdated += OnPathChanged;
+            }
+        }
+        public void ResetPathFollow()
+        {
+            distanceTravelled = 0f;
+        }
+        #endregion
+    }
+}
