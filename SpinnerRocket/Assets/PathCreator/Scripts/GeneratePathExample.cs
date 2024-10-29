6648021056b0dae1c7d2e64d18a82950bf99efbe
guillermo <guille_medina575@hotmail.com>
2024-10-29 00:31:53 -0600

    ajustes scenes nuevos

--- SpinnerRocket/Assets/PathCreator/Scripts/GeneratePathExample.cs
+++ SpinnerRocket/Assets/PathCreator/Scripts/GeneratePathExample.cs
@@ -4,10 +4,12 @@ namespace PathCreation.Examples
     [RequireComponent(typeof(PathCreator))]
     public class GeneratePathExample : MonoBehaviour {
         public Transform[] waypoints;
+        public bool ClosePath;
+        public PathCreation.PathSpace pathSpace = PathCreation.PathSpace.xy;
         void Start () {
             if (waypoints.Length > 0) {
                 var objPath = GetComponent<PathCreator>();
-                BezierPath bezierPath = new BezierPath (waypoints);
+                BezierPath bezierPath = new BezierPath (waypoints, ClosePath, pathSpace);
                 GetComponent<PathCreator> ().bezierPath = bezierPath;
             }
         }
